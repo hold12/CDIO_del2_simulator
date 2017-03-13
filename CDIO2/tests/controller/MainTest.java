fe90c4d6260d37d210aa8b92ae3c6141c6f6e98a
Anders W. Olsen <anders@wiberg.tech>
2017-03-13 17:24:32 +0100

    Adding Travis

---
+++ CDIO2/tests/controller/MainTest.java
@@ -0,0 +1,16 @@
+package controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by AndersWOlsen on 13-03-2017.
+ */
+class MainTest {
+    @Test
+    void main() {
+        assertTrue(true);
+    }
+
+}
