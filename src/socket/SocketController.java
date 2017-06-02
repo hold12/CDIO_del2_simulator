6e3f601f92b97f43ff583d46e79d6193032eac47
Freya Helstrup <freyagh@me.com>
2017-06-02 15:37:44 +0200

    Added "I4 A" message

--- src/socket/SocketController.java
+++ src/socket/SocketController.java
@@ -46,8 +46,9 @@ public class SocketController implements ISocketController {
 		//TODO some logic for listening to a socket //(Using try with resources for auto-close of socket)
  		try (ServerSocket listeningSocket = new ServerSocket(port)){
 			while (true){
-				waitForConnections(listeningSocket);
-			}
+                sendMessage(new SocketOutMessage("I4 A")); //First message after turning on the weight
+                waitForConnections(listeningSocket);
+            }
 		} catch (IOException e1) {
 			// TODO Maybe notify MainController?
 			e1.printStackTrace();
@@ -65,9 +66,10 @@ public class SocketController implements ISocketController {
 			//.readLine is a blocking call
 			//TODO How do you handle simultaneous input and output on socket?
 			//TODO this only allows for one open connection - how would you handle multiple connections?
-			while (true){
-				inLine = inStream.readLine();
-				if (inLine==null) break;
+
+            while (true) {
+                inLine = inStream.readLine();
+                if (inLine==null) break;
 				switch (inLine.split(" ")[0]) {
 				case "RM20": // Display a message in the secondary display and wait for response
 					if (inLine.split(" ")[1].equals("8"))
